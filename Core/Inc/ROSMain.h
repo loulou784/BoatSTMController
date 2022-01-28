904cdfa31bd3c33bad6e334334ab6eabcb6e2607
Alex Morin <alexmorin68@outlook.com>
2022-01-28 16:48:26 -0500

    Initial commit, VCP, LED and Buttons are configured, added ROSSerial library. ROSSerial with VCP not working

---
+++ Core/Inc/ROSMain.h
@@ -0,0 +1,22 @@
+/*
+ * main.hpp
+ *
+ *  Created on: 2018/01/17
+ *      Author: yoneken
+ */
+
+#ifndef __ROSMAIN_H_
+#define __ROSMAIN_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+void setup(void);
+void loop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ROSMAIN_H_ */
